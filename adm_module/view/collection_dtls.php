eb0c79ea00ae9b4ba69b586878ff528a2c168f8e
joypaul1 <jyla5102555@gmail.com>
2024-05-28 17:06:31 +0600

    Collection_Report

--- adm_module/view/collection_dtls.php
+++ adm_module/view/collection_dtls.php
@@ -27,7 +27,7 @@ $end_date   = $_REQUEST['end_date'];
 				<div class="col-lg-12">
 					<div class="md-form">
 						<div class="resume-item d-flex flex-column flex-md-row">
-							<table class="table table-striped table-bordered table-sm" id="table" cellspacing="0" width="100%">
+							<table class="table table-striped table-bordered table-sm" id="tbl" cellspacing="0" width="100%">
 								<thead class="bg-light">
 									<tr>
 										<th scope="col">Sl</th>
